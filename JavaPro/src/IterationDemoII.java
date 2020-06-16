7e6f22d894fd8ae578f0ed10f7e54a6d4b7f261f
444 <444@192.168.0.9>
2020-06-16 21:05:21 +0530

    ForLoop another example for Printing same Numbers

---
+++ JavaPro/src/IterationDemoII.java
@@ -0,0 +1,16 @@
+//ForLoop another example for Printing same Numbers
+public class IterationDemoII
+{
+public static void main(String[] args)
+{
+		for(int j=1;j<=4;j++)
+		{
+			for(int i=1;i<=5;i++)
+			{
+				System.out.print(j +" ");
+				
+			}
+			System.out.println();
+		}
+	}
+}
