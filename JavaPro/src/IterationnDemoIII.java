43a0c75f1fd2b69b752b28f19b85ec284895a362
444 <444@192.168.0.9>
2020-06-16 21:08:05 +0530

    ForLoop another example for Printing 1234 Numbers

---
+++ JavaPro/src/IterationnDemoIII.java
@@ -0,0 +1,19 @@
+////ForLoop another example for Printing 1234 Numbers
+
+public class IterationnDemoIII
+{
+	public static void main(String[] args)
+	{
+			for(int j=1;j<=4;j++)
+			{
+				for(int i=1;i<=5;i++)
+				{
+					System.out.print(i +" ");
+					
+				}
+				System.out.println();
+			}
+		}
+	
+}
+
