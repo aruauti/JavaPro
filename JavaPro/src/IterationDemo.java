4861abaf25d47503a32381fbfd309856fa7ead0f
444 <444@192.168.0.9>
2020-06-16 20:38:41 +0530

    While loop program

---
+++ JavaPro/src/IterationDemo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * while loop Program
+ *
+ */
+public class IterationDemo
+{
+public static void main(String[] args)
+{
+
+int i=1;
+while (i<=5)
+{
+	System.out.println("Hello");
+	i++;
+	}
+
+}
+}
