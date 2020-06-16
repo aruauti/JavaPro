38d4c49d7d7ff63ac980fadcdaef0a4ce00969da
444 <444@192.168.0.9>
2020-06-16 13:34:23 +0530

    First Commit

---
+++ JavaPro/src/Javapro1.java
@@ -0,0 +1,15 @@
+
+public class Javapro1{
+	public static void main(String args[])
+	{
+		int n = 5;
+		
+		if(n==0)
+				System.out.print("nothing");	
+		else if (n%2==0)  
+			System.out.print("Even");	
+		else
+			System.out.print("Odd");
+	}
+	
+}
